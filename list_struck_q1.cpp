38dffa6f5629553ecc2386696f2f4c17ec9c54f0
Carol <caroldazevedo52@gmail.com>
2025-05-28 17:59:39 -0300

    Add files via upload

---
+++ list_struck_q1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+struct Ponto{
+    int x, y;
+};
+
+int main(){
+    Ponto p;
+    int b;
+    cin >> p.x >> p.y >> std::endl;
+    cin >> b >> std::endl;
+    struct Ponto pontos[b];
+    
+    for (int i=0; i<x ; i++){
+        cin >> p.x >> p.y >> pontos[i].Ponto >> std::endl;
+
+    }
+}
