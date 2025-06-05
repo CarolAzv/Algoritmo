517f7a907032578357e9217cf4e9d005baf807a7
Carol <caroldazevedo52@gmail.com>
2025-06-04 23:54:52 -0300

    Add files via upload

--- Lista 3/list3_q1.cpp
+++ Lista 3/list3_q1.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-//randi para a outra questão (q4)
-//função de escolher a corverção
 
 int main(){
-    double f, c, t;
+    double tempnow, tempf, t;
     int qual;
+
+    std::cout << "Diga sua temperatura: ";
+    std::cin >> tempnow;
+    std::cout << "Selecione (1) para converter Fahrenheit para Celsius";
+    std::cout << "Selecione (2) para converter Celsius para Fahrenheit";
+    std::cin >> qual;
+
+    if(qual = 1){
+        tempf = (tempnow - 32) * 1.8;
+        return tempf;
+    }
+    else if(qual = 2){
+        tempf = (tempnow * 1.8) + 32;
+        return tempf;
+    }
+    std::cout << "A temperatura convertida é " << tempf << "º";
+
+    return 0;
 }
---
+++ Lista 3/list3_q5.end.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#include <ctime>
+clock_t startTimer;
+time_t beginTimer;
+void tic()
+{
+ beginTimer = time(&beginTimer);
+ struct tm * timeinfo;
+ timeinfo = localtime(&beginTimer);
+ std::cout << "simulation start at: " << asctime(timeinfo) << std::endl;
+}
+double toc()
+{
+ time_t finishTimer = time(&finishTimer);
+ double simTime = difftime(finishTimer, beginTimer) / 60.0;
+ struct tm * timeinfo;
+ timeinfo = localtime(&finishTimer);
+ std::cout << "simulation finished at: " << asctime(timeinfo) << std::endl;
+ std::cout << "Time elapsed: " << simTime << " minutes" << std::endl;
+ return simTime;
+}
+
+//=========================================================================//
+int main(){
+    double velo, dist, temp;
+
+    std::cout << "Distancia pecorida: ";
+    std::cin >> dist;
+    std::cout << "Tempo de viagem: ";
+    std::cin >> temp;
+
+    velo = dist/temp;
+    std::cout << "A velocidade media é: " << velo;
+    
+    return 0;
+}
