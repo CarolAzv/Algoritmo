9eb608a95654cbeb00db9d38e2f6e78c16cd4ad3
Carol <caroldazevedo52@gmail.com>
2025-06-09 14:08:36 -0300

    Update Candidato.cpp

--- trabalho em aula/Estruturas_execicio_p1/Candidato.cpp
+++ trabalho em aula/Estruturas_execicio_p1/Candidato.cpp
@@ -11,6 +11,13 @@ using std::ifstream;
 using std::ofstream;
 using std::ios;
 
+//Guarda o nome dos candidatos
+char candidatoNome[MAXCANDIDATO][MAXNOME];
+//Guarda o numero dos candidatos
+int candidatoNumero[MAXCANDIDATO];
+//Guarda quantos votos cada candidato teve
+int candidatoVoto[MAXCANDIDATO];
+
 const int MAXNOME = 100;
 const int MAXCANDIDATO = 20;
 
@@ -19,8 +26,22 @@ struct Candidato{
     int numero;
     int votos;
 };
+  
+ifstream entrada("candidatos.txt");
+    if(!entrada.is_open){
+        cout << "Error ao abrir o arquivo" << endl;
+        exit(0);
+    }
+    int numCandidatos = 0;
+    while (entrada >> cand[numCandidatos].numero) {
+        entrada.getline(cand[numCandidatos].nome, MAXNOME);
+        cand[numCandidatos].votos = 0;
+        numCandidatos++;
+    }
+    entrada.close();
 
-int main() {
+
+int main(){
     Candidato cand[MAXCANDIDATO];
     char arquivoEntrada[] = "candidatos.txt";
     char arquivoSaida[] = "votos.txt";
@@ -28,7 +49,7 @@ int main() {
     //Simula leCandidatos()
     ifstream entrada(arquivoEntrada);
     if(!entrada.is_open()){
-        cout << "Erro ao abrir arquivo " << arquivoEntrada << endl;
+        cout << "Erro ao abrir arquivo " << arquivoEntrada;
         exit(0);
     }
 
@@ -48,25 +69,26 @@ int main() {
         }
     }
 
-    cout << "O candidato vencedor foi " << cand[vencedor].nome << endl;
+    cout << "O candidato vencedor foi " << cand[vencedor].nome;
     return 0;
 
     //Loop de votos
     int voto = 0;
     while(voto != -1){
         //tem que ler as informações dos candiatos, como?
-        cout << "Em qual candidato você quer votar?" << endl;
-        cout << "----------------------------------" << endl;
-        cout << "| " << arumar << endl;
-        cout << "| " << arumar << endl;
-        cout << "| " << arumar << endl;
-        cout << "----------------------------------" << endl;
+        cout << "Em qual candidato você quer votar?";
+        cout << "----------------------------------";
+        for(int i=2; i<numCandidatos; i++){
+            cout << "| ";
+        }
+        cout << "----------------------------------";
         cin >> voto;
+    }
 
         //Atualização dos votos
         ifstream saida(arquivoSaida);
         if(!entrada.is_open()){
-            cout << "Erro ao abrir arquivo " << arquivoSaida << endl;
+            cout << "Erro ao abrir arquivo " << arquivoSaida;
             exit(0);
         }
 
@@ -78,5 +100,4 @@ int main() {
             //numCandidatos++;
         }
         saida.close();
-    }
 }
