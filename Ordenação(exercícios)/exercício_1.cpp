9da3777f5b615ac3258ec1b5b07bdb5b88bfac2c
Carol <caroldazevedo52@gmail.com>
2025-06-24 23:02:19 -0300

    Update and rename exercício_1(notyet).cpp to exercício_1.cpp

--- Ordenação(exercícios)/exercício_1.cpp
+++ Ordenação(exercícios)/exercício_1.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void troca(int *v1, int *v2){
+void troca(int v1, int v2){
     int salva;
-    salva = *v2;
-    *v2 = *v1;
-    *v1 =  salva;
+    salva = v2;
+    v2 = v1;
+    v1 =  salva;
 }
 
-void temduplo(int v, int n){
+bool temduplo(int v[], int n){
     for(int i=1; i<n; i++){
         if(v[i] == v[i-1]){
             return true;
@@ -17,29 +17,39 @@ void temduplo(int v, int n){
     return false;
 }
 
-void imprimir(int lista, int n){
+void imprimir(int v[], int n){
     for(int i=0; i<n; i++){
-        cout << lista[i] << " ";
+        cout << v[i] << " ";
     }
 }
 
 int main(){
-    int n, v;
+    int n;
+    int v[n];
+    bool tem;
     cout << "Quantos numeros tem a array: ";
     cin >> n;
     cout << "Digite os numeros: ";
+    cin >> v[0];
     for(int i=1; i<n; i++){
-        cin >> v;
+        cout << " ";
+        cin >> v[i];
     }
 
     for(int i=1; i<n; i++){
-        int j = i;
+        int j=i;
         while(j>0 && v[j] < v[j-1]){
             troca(v[j], v[j-1]);
             j = j-1;
         }
     }
-
+    tem = temduplo;
+    if(tem = true){
+        cout << "Verdadeiro";
+    }
+    if(tem = false){
+        cout << "Falso";
+    }
     imprimir(v, n);
     return 0;
 }
