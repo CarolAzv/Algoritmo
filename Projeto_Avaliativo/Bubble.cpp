37a2669e879aba776fba02e978cbf6228c7a50e8
Carol <caroldazevedo52@gmail.com>
2025-06-30 13:08:12 -0300

    Create Bubble.cpp

---
+++ Projeto_Avaliativo/Bubble.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int numero=0; //numeros fechados
+    char letra='A'; //uma unica letra
+    double decimal1=10.9999999; //um numero decimal preciso
+    float decimal2=10.5; //um numero decimal pouco preciso
+    bool sim=true; //true=verdade, false=falso
+    string nome="Bruno"; //palavras e frases
+
+    cout << numero << "\n";
+    cout << letra << "\n";
+    cout << decimal1 << "\n";
+    cout << decimal2 << "\n";
+    cout << sim << "\n";
+    cout << nome << "\n";
+    
+    return 0;
+}
