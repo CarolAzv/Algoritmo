c67683ef5b71c6823d61665a6359b4c475911334
Carol <caroldazevedo52@gmail.com>
2025-07-02 23:06:49 -0300

    Update Bubble.cpp

--- Projeto_Avaliativo/Bubble.cpp
+++ Projeto_Avaliativo/Bubble.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
-
 using namespace std;
-int main(){
-    int numero=0; //numeros fechados
-    double decimal1=10.9999999; //um numero decimal preciso
-    float decimal2=10.5; //um numero decimal pouco preciso
 
-    cout << numero << "\n";
-    cout << decimal1 << "\n";
-    cout << decimal2 << "\n";
-    
+void bubbleSort(int a[], int m){
+    for (int i = 0; i < m - 1; i++){
+        for (int j = 0; j < m - i - 1; j++){
+            if (a[j] > a[j + 1]){
+                swap(a[j], a[j + 1]);
+            }
+        }
+    }
+}
+
+int main(){
+    int array[300], tamanho, temp;
+    cout << "informe o tamanha da array: ";
+    cin >> tamanho;
+    cout << '\n' << "informe a array: ";
+
+    for(int i=0; i<tamanho; i++){
+        cin >> temp;
+        array[i] = temp;
+    }
+
+    bubbleSort(array, tamanho);
+    for(int i=0; i<tamanho; i++){
+        cout << array[i] << " ";
+    }
     return 0;
 }
