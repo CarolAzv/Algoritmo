d7770e53ae3369f177bc85d57a7630029238e22c
Carol <caroldazevedo52@gmail.com>
2025-07-14 13:20:23 -0300

    Update Fatorial.cpp

--- Projeto_Avaliativo_2/Fatorial.cpp
+++ Projeto_Avaliativo_2/Fatorial.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int Fatorial(int a, int b, int mul){
+  if(b <= 0){
+    return 0
+  }
+  else{
+    mul= a*b
+      return mul + Fatorial(a-2, b-2, mul)
+  }
+}
+
+int main(){
+    int numero=0; //numeros fechados
+
+    cout << numero << "\n";
+    
+    return 0;
+}
