e7666bb1c8b49e514cf6abe890531b967ee17741
Carol <caroldazevedo52@gmail.com>
2025-07-14 14:02:28 -0300

    Update MinMax.cpp

--- Projeto_Avaliativo_2/MinMax.cpp
+++ Projeto_Avaliativo_2/MinMax.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
-using namespace ust;
+using namespace std;
+
+int minmax(){}
+
+int main(){
+  int n;
+
+  cout << "Informe o tamanho da array: ";
+  cin >> n;
+  int array[n];
+  
+  return 0;
+}
