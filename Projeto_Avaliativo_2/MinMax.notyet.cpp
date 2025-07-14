7c8c5b3f6ffe7bd3bae1ac3959335ca44fa2d511
Carol <caroldazevedo52@gmail.com>
2025-07-14 14:04:01 -0300

    Update and rename MinMax.cpp to MinMax.notyet.cpp

--- Projeto_Avaliativo_2/MinMax.notyet.cpp
+++ Projeto_Avaliativo_2/MinMax.notyet.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+void randomarray(int array[], int tamanho){
+  for (int i = 0; i < tamanho; ++i){
+    array[i] = rand() % 1001;
+  }
+}
+
 int minmax(){}
 
 int main(){
@@ -9,6 +15,7 @@ int main(){
   cout << "Informe o tamanho da array: ";
   cin >> n;
   int array[n];
-  
+
+  randomarray(array, n);
   return 0;
 }
