0dd0d7eaa0753efc77e71c6eba929c96ee5ff781
Carol <caroldazevedo52@gmail.com>
2025-07-15 22:22:43 -0300

    Update MinMax.notyet.cpp

--- Projeto_Avaliativo_2/MinMax.notyet.cpp
+++ Projeto_Avaliativo_2/MinMax.notyet.cpp
@@ -7,15 +7,37 @@ void randomarray(int array[], int tamanho){
   }
 }
 
-int minmax(){}
+int minmax(int array,  int tamanho){
+  int min, max, minmax[2];
+  min = array[0];
+  max = array[0];
+  if(array[1] > max){
+    max = array[1];
+  }
+  if(array[1] < min){
+    min = array[1];
+  }
+  for(i = 2; i<tamanho; i++){
+    if(array[1] > max){
+      max = array[1];
+    }
+    if(array[1] < min){
+      min = array[1];
+    }
+  }
+  minmax[0] = min;
+  minmax[1] = max;
+  return minmax;
+}
 
 int main(){
-  int n;
+  int n, minmax[2];
 
   cout << "Informe o tamanho da array: ";
   cin >> n;
   int array[n];
 
   randomarray(array, n);
+  minmax = minmax(array,  n);
   return 0;
 }
