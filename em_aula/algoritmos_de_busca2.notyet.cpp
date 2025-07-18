4cd0c207aae86f1700f97dcccafa155320e94080
Carol <caroldazevedo52@gmail.com>
2025-07-18 15:40:46 -0300

    Update algoritmos_de_busca2.notyet.cpp

--- em_aula/algoritmos_de_busca2.notyet.cpp
+++ em_aula/algoritmos_de_busca2.notyet.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
-#include <bits/stdc++.h>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 #include <random>
+#include <chrono>
 using namespace std;
 
+void tic(int mode=0) {
+    static std::chrono::_V2::system_clock::time_point t_start;
+    
+    if (mode==0)
+        t_start = std::chrono::high_resolution_clock::now();
+    else {
+        auto t_end = std::chrono::high_resolution_clock::now();
+        std::cout << "Elapsed time is " << (t_end-t_start).count()*1E-9 << " seconds\n";
+    }
+}
+void toc() { tic(1); }
+
 void randomArray(int array[], int tamanho){
-  srand(time(0));
   for (int i = 0; i < tamanho; ++i){
     array[i] = rand() % 10001; 
   }
@@ -76,50 +90,71 @@ int linear(int array[], int tamanho, int chave){
   return -1;
 }
 
-int binarioR(int array[], int comeco, int fim, int chave){
-  int meio = (comeco+(fim-comeco))/2;
-  if (array[meio] == chave){
-    return meio;
-  }
-  
-  if(array[meio]<chave){
-    return buscaBinariaRecursiva(arr, chave, meio + 1, fim);
-  }
-  
-  return buscaBinariaRecursiva(arr, chave, inicio, meio - 1);
+int linearR(int array[], int indice, int chave){
+   if indice == len(lista):
+        return -1
+    if lista[indice] == chave:
+        return indice
+    return linearR(lista, alvo, indice + 1)
 }
 
-int binarioI(int array[], int comeco, int fim, int chave){
+int binarioL(int array[], int comeco, int fim, int chave){
   int meio;
   while(comeco<=fim){
-    meio = (comeco+(fim-comeco))/2;
+    meio = comeco+(fim-comeco)/2;
     if(array[meio]==chave){
       return meio;
     }
     if(chave < array[meio]){
       fim = meio-1;
     }
-    if(chave > array[meio]){
+    else if(chave > array[meio]){
       comeco = meio+1;
     }
   }
   return -1;
 }
 
+int binarioR(int array[], int comeco, int fim, int chave){
+  int meio = inicio + (fim - inicio) / 2;
+  
+  if (comeco > fim){
+    return -1;
+  }
+  if (array[meio] == chave){
+    return meio;
+  }
+  else if (array[meio] < chave){
+    return binarioR(arr, chave, meio + 1, fim);
+  }
+  else{
+    return binarioR(arr, chave, comeco, meio - 1);
+  }
+}
+
 int main(){
-  int tamanho = 10000, array[10000], chave, resli, resbi;
+  int tamanho = 10000, array[10000], chave, resli, resbiL, resbiR;
+  srand(time(0));
   chave = randomnum();
   randomArray(array, tamanho);
 
   cout << "A chave áleatoria é: " << chave << '\n';
 
+  tic();
   resli = linear(array, tamanho, chave);
+  toc();
   cout << "Resultado da busca linear: " << resli << '\n';
 
+  tic();
   mergeSort(array, 0, tamanho - 1);
-  cout << "sort done";
-  resbi = binario(array, 0, tamanho-1, chave);
-  cout << "Resultado da busca binaria: " << resbi;
+
+  resbiL = binarioL(array, 0, tamanho-1, chave);
+  toc();
+  cout << "Resultado da busca binaria linear: " << resbiI;
+
+  resbiR = binarioR(array, 0, tamanho-1, chave);
+  toc();
+  cout << "Resultado da busca binaria recursiva: " << resbiR;
 
   return 0;
 }
