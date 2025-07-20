f4c9ea630ce677d130c5924490c230c48fd4fb8c
Carol <caroldazevedo52@gmail.com>
2025-07-20 16:43:09 -0300

    Create q_2.cpp

---
+++ Lista de Exercícios 2-1/q_2.cpp
@@ -0,0 +1,31 @@
+#include <iostram>
+using namespace std;
+
+int soma(int num1, int num2){
+  int resultado = num1 + num2;
+  return resultado;
+}
+
+int main(){
+  int num1, num2, result, again;
+
+  cout << "Digite o primerio numero para a soma: ";
+  cin >> num1;
+
+  cout << "Digite o segundo numero para a soma: ";
+  cin >> num2;
+
+  result = soma(num1, num2);
+  cout << '\n' << "Resultado: " << result << '\n';
+
+  cout << "Digite 1 para rodar o programa novamente: ";
+  cin >> again;
+
+  if(again==1){
+    return main;
+  }
+  else{
+    return 0;
+  }
+  
+}
