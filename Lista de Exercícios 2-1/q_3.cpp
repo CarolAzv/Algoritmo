5f7afbb8c4bfba1c86051299aab23c623a9f19a8
Carol <caroldazevedo52@gmail.com>
2025-07-20 16:45:36 -0300

    Update q_3.cpp

--- Lista de Exercícios 2-1/q_3.cpp
+++ Lista de Exercícios 2-1/q_3.cpp
@@ -1,4 +1,21 @@
 #include <iostream>
 using namespace std;
 
+long long fibonacci(int n, int a, int b, int check){
+  long long fib;
+  check++;
+  fib = a + b;
+  a = b;
+  b = fib;
+  if(n == 1){
+    return 1;
+  }
+  if(n == check){
+    return a;
+  }
+  else{
+    return fibonacci(n, a, b, check);
+  }
+}
+
 int main(){}
