b7eb957b531396a5977fe552fa3262265a6e3537
Carol <caroldazevedo52@gmail.com>
2025-07-20 16:34:17 -0300

    Update q_1.cpp

--- Lista de Exercícios 2-1/q_1.cpp
+++ Lista de Exercícios 2-1/q_1.cpp
@@ -1,1 +1,24 @@
 #include <iostream>
+using namespace std;
+
+int somar(int n, int fim, int check){
+  if(n==check){
+    return fim
+  }
+  else{
+    fim = fim + check;
+    check++;
+    return somar(n, fim, check);
+  }
+}
+
+int main(){
+  int n, res;
+
+  cout << "Digite até qual numero você quer somar: ";
+  cin >> n;
+
+  res = somar(n, 0, 0);
+  cout << '\n' << "Resultado: " << res;
+  return 0;
+}
