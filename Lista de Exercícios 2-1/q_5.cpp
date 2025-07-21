1c198773e1c3d48872568768e3c1617b552b70be
Carol <caroldazevedo52@gmail.com>
2025-07-21 13:46:15 -0300

    Update q_5.cpp

--- Lista de Exercícios 2-1/q_5.cpp
+++ Lista de Exercícios 2-1/q_5.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 void randomArray(int array[], int tamanho){
@@ -7,18 +8,65 @@ void randomArray(int array[], int tamanho){
   }
 }
 
-int randomKey(int tamanho, int array){
+int randomKey(int tamanho){
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<> dis(0, tamanho);
     int randomNumber = dis(gen);
 
-  return array[randomNumber];
+    return randomNumber;
+}
+
+void merge(int array[], int left, int mid, int right){
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    vector<int> L(n1), R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = array[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = array[mid + 1 + j];
+
+    int i = 0, j = 0;
+    int k = left;
+
+    while(i < n1 && j < n2){
+        if (L[i] <= R[j]) {
+            array[k] = L[i];
+            i++;
+        }
+        else{
+            array[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i < n1){
+        array[k] = L[i];
+        i++;
+        k++;
+    }
+    while(j < n2){
+        array[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int array[], int left, int right){
+    if (left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+    mergeSort(array, left, mid);
+    mergeSort(array, mid + 1, right);
+    merge(array, left, mid, right);
 }
 
 int binarioR(int array[], int comeco, int fim, int chave){
-  int meio = comeco + (fim - comeco) / 2;
-  
+  int meio = comeco + fim/ 2;
+
   if (comeco > fim){
     return -1;
   }
@@ -26,33 +74,41 @@ int binarioR(int array[], int comeco, int fim, int chave){
     return meio;
   }
   else if (array[meio] < chave){
-    return binarioR(array, chave, meio + 1, fim);
+    return binarioR(array, meio + 1, fim, chave);
   }
   else{
-    return binarioR(array, chave, comeco, meio - 1);
+    return binarioR(array, comeco, meio - 1, chave);
   }
 }
 
 int main(){
-  int tamanho, chave, resbiR;
+  int tamanho, chave, resbiR, check;
   srand(time(0));
   
-  cout << "Digit o tamanho do vetor: " << chave << '\n';
+  cout << "Digit o tamanho do vetor: " << '\n';
   cin >> tamanho;
   int array[tamanho];
   
   randomArray(array, tamanho);
-  chave = randomKey(tamanho-1, array);
+  chave = randomKey(tamanho-1);
+  chave = array[chave];
   
-  cout << "A chave áleatoria é: " << chave << '\n';
 
   mergeSort(array, 0, tamanho - 1);
-  for(int i=0; i<tamanho; i++){
-        array[i] = array2[i];
-    }
 
-  resbiR = binarioR(array2, 0, tamanho-1, chave);
+  resbiR = binarioR(array, 0, tamanho, chave);
   cout << "Resultado da busca Binaria Recursiva: " << resbiR;
 
+  cout << "1 Para checkar resultado: ";
+  cin >> check;
+
+  if(check==1){
+    cout << "A chave áleatoria é: " << chave << '\n';
+    cout << '\n';
+    for(int i=0; i<tamanho; i++){
+      cout << array[i] << " ";
+    }
+  }
+  
   return 0;
 }
