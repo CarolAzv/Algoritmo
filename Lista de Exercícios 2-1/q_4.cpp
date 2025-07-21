799fdd6d30d836ee1d3a459b81878b9774ea529f
Carol <caroldazevedo52@gmail.com>
2025-07-20 23:55:59 -0300

    Update q_4.cpp

--- Lista de Exercícios 2-1/q_4.cpp
+++ Lista de Exercícios 2-1/q_4.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 using namespace std;
 
+void randomArray(int array[], int tamanho){
+  srand(time(0));
+  for (int i = 0; i < tamanho; ++i){
+    array[i] = rand() % 1001; 
+  }
+}
+
 int main(){
   int tamanho;
 
   cout << "Tamanho do vetor: ";
   cin >> tamanho;
   int vetor[tamanho];
+  randomArray(vetor, tamanho);
   
   return 0;
 }
