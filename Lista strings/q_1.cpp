d08806cd09428cf132b4d94a9d6ea0cfd546a2e7
Carol <caroldazevedo52@gmail.com>
2025-07-25 17:54:02 -0300

    Update q_1.cpp

--- Lista strings/q_1.cpp
+++ Lista strings/q_1.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 void divisor(char letras[], char vogais[], char consoantes[]){
-  for(int b = 0; b<strlen(letras)-1; b++){
-    if(letras[b]==a || letras[b]==e || letras[b]==i || letras[b]==o || letras[b]==u){
-      vogais.push_back(letras[b]);
+    for(int b=0; b<strlen(letras)-1; b++){
+        char temp[2];
+        temp[0] = letras[b];
+        temp[1] = '\0';
+        if(letras[b]==char("a") || letras[b]==char("e") || letras[b]==char("i") || letras[b]==char("o") || letras[b]==char("u")){
+            strcat(vogais, temp);
+        }
+        else{
+            strcat(consoantes, temp);
+        }
     }
-    else{
-      consoantes.push_back(letras[b]);
-    }
-  }
 }
 
 int main(){
-  char letras[1000], vogais[1000], consoantes[1000];
-  cout << "Digite uma palavra ou frase: ";
-  cin >> letras;
+    char letras[1000], vogais[1000], consoantes[1000];
+    cout << "Digite uma palavra ou frase: ";
+    cin >> letras;
 
-  divisor(letras, vogais, consoantes);
+    divisor(letras, vogais, consoantes);
 
-  for(int i=0; i<strlen(vogais)-1; i++){
-    cout << vogais[i] << " ";
-  }
-  for(int g=0; g<strlen(consoantes)-1; g++){
-    cout << consoantes[g] << " ";
-  }
+    for(int i=0; i<strlen(vogais)-1; i++){
+      cout << vogais[i] << " ";
+     }
+    for(int g=0; g<strlen(consoantes)-1; g++){
+       cout << consoantes[g] << " ";
+     }
 
-  return 0;
+    return 0;
 }
