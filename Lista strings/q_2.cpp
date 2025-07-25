e3e51833ff39ba461970cc947a2ab9eb659d0809
Carol <caroldazevedo52@gmail.com>
2025-07-25 15:09:35 -0300

    Update q_2.cpp

--- Lista strings/q_2.cpp
+++ Lista strings/q_2.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main(){
+  char letras[1000];
+  cout << "Digite uma palavra ou frase: ";
+  cin.getline(letras);
+
+  for(int i=0; i<strlen(letras); i++){
+    if(letras[i]==" "){
+      cout << '\n';
+    }
+    else{
+      cout << letras[i];
+    }
+  }
+  
   return 0
 }
