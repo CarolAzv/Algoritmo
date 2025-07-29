4515df2533e471750d994f900f08f704b0176e14
Carol <caroldazevedo52@gmail.com>
2025-07-28 21:18:56 -0300

    Update q_4.cpp

--- Lista strings/q_4.cpp
+++ Lista strings/q_4.cpp
@@ -1,73 +1,79 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 void contador(char letras[], int alfabeto[]){
-  for(int i; i<strlen(letras); i++){
-    if(letras[i]=="a" || letras[i]=="A"){
-      alfabeto[0] = alfabeto[0]+1}
-    else if(letras[i]=="b" || letras[i]=="B"){
-      alfabeto[1] = alfabeto[1]+1}
-    else if(letras[i]=="c" || letras[i]=="C"){
-      alfabeto[2] = alfabeto[2]+1}
-    else if(letras[i]=="d" || letras[i]=="D"){
-      alfabeto[3] = alfabeto[3]+1}
-    else if(letras[i]=="e" || letras[i]=="E"){
-      alfabeto[4] = alfabeto[4]+1}
-    else if(letras[i]=="f" || letras[i]=="F"){
-      alfabeto[5] = alfabeto[5]+1}
-    else if(letras[i]=="g" || letras[i]=="G"){
-      alfabeto[6] = alfabeto[6]+1}
-    else if(letras[i]=="h" || letras[i]=="H"){
-      alfabeto[7] = alfabeto[7]+1}
-    else if(letras[i]=="i" || letras[i]=="I"){
-      alfabeto[8] = alfabeto[8]+1}
-    else if(letras[i]=="j" || letras[i]=="J"){
-      alfabeto[9] = alfabeto[9]+1}
-    else if(letras[i]=="k" || letras[i]=="K"){
-      alfabeto[10] = alfabeto[10]+1}
-    else if(letras[i]=="l" || letras[i]=="L"){
-      alfabeto[11] = alfabeto[11]+1}
-    else if(letras[i]=="m" || letras[i]=="M"){
-      alfabeto[12] = alfabeto[12]+1}
-    else if(letras[i]=="n" || letras[i]=="N"){
-      alfabeto[13] = alfabeto[13]+1}
-    else if(letras[i]=="o" || letras[i]=="O"){
-      alfabeto[14] = alfabeto[14]+1}
-    else if(letras[i]=="p" || letras[i]=="P"){
-      alfabeto[15] = alfabeto[15]+1}
-    else if(letras[i]=="q" || letras[i]=="Q"){
-      alfabeto[16] = alfabeto[16]+1}
-    else if(letras[i]=="r" || letras[i]=="R"){
-      alfabeto[17] = alfabeto[17]+1}
-    else if(letras[i]=="s" || letras[i]=="S"){
-      alfabeto[18] = alfabeto[18]+1}
-    else if(letras[i]=="t" || letras[i]=="T"){
-      alfabeto[19] = alfabeto[19]+1}
-    else if(letras[i]=="u" || letras[i]=="U"){
-      alfabeto[20] = alfabeto[20]+1}
-    else if(letras[i]=="v" || letras[i]=="V"){
-      alfabeto[21] = alfabeto[21]+1}
-    else if(letras[i]=="w" || letras[i]=="W"){
-      alfabeto[22] = alfabeto[22]+1}
-    else if(letras[i]=="x" || letras[i]=="X"){
-      alfabeto[23] = alfabeto[23]+1}
-    else if(letras[i]=="y" || letras[i]=="Y"){
-      alfabeto[24] = alfabeto[24]+1}
-    else if(letras[i]=="z" || letras[i]=="Z"){
-      alfabeto[25] = alfabeto[25]+1}
-  }
+    char check[27]="abcdefghijklmnopqrstuvwxyz";
+    char CHECK[27]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for(int i=0; i<strlen(letras); i++){
+        if(letras[i]==check[0] || letras[i]==CHECK[0]){
+            alfabeto[0]++;}
+        else if(letras[i]==check[1] || letras[i]==CHECK[1]){
+            alfabeto[1]++;}
+        else if(letras[i]==check[2] || letras[i]==CHECK[2]){
+            alfabeto[2]++;}
+        else if(letras[i]==check[3] || letras[i]==CHECK[3]){
+            alfabeto[3]++;}
+        else if(letras[i]==check[4] || letras[i]==CHECK[4]){
+            alfabeto[4]++;}
+        else if(letras[i]==check[5] || letras[i]==CHECK[5]){
+            alfabeto[5]++;}
+        else if(letras[i]==check[6] || letras[i]==CHECK[6]){
+            alfabeto[6]++;}
+        else if(letras[i]==check[7] || letras[i]==CHECK[7]){
+            alfabeto[7]++;}
+        else if(letras[8]==check[0] || letras[i]==CHECK[9]){
+            alfabeto[8]++;}
+        else if(letras[9]==check[0] || letras[i]==CHECK[9]){
+            alfabeto[9]++;}
+        else if(letras[i]==check[10] || letras[i]==CHECK[10]){
+            alfabeto[10]++;}
+        else if(letras[i]==check[11] || letras[i]==CHECK[11]){
+            alfabeto[11]++;}
+        else if(letras[i]==check[12] || letras[i]==CHECK[12]){
+            alfabeto[12]++;}
+        else if(letras[i]==check[13] || letras[i]==CHECK[13]){
+            alfabeto[13]++;}
+        else if(letras[i]==check[14] || letras[i]==CHECK[14]){
+            alfabeto[14]++;}
+        else if(letras[i]==check[15] || letras[i]==CHECK[15]){
+            alfabeto[15]++;}
+        else if(letras[i]==check[16] || letras[i]==CHECK[16]){
+            alfabeto[16]++;}
+        else if(letras[i]==check[17] || letras[i]==CHECK[17]){
+            alfabeto[17]++;}
+        else if(letras[i]==check[18] || letras[i]==CHECK[18]){
+            alfabeto[18]++;}
+        else if(letras[i]==check[19] || letras[i]==CHECK[19]){
+            alfabeto[19]++;}
+        else if(letras[i]==check[20] || letras[i]==CHECK[20]){
+            alfabeto[20]++;}
+        else if(letras[i]==check[21] || letras[i]==CHECK[21]){
+            alfabeto[21]++;}
+        else if(letras[i]==check[22] || letras[i]==CHECK[22]){
+            alfabeto[22]++;}
+        else if(letras[i]==check[23] || letras[i]==CHECK[23]){
+            alfabeto[23]++;}
+        else if(letras[i]==check[24] || letras[i]==CHECK[24]){
+            alfabeto[24]++;}
+        else if(letras[i]==check[25] || letras[i]==CHECK[25]){
+            alfabeto[25]++;}
+    }
 }
 
 int main(){
   char letras[1000];
   int alfabeto[26];
+  for(int i=0;i<26;i++){
+    alfabeto[i]=0;
+  }
   cout << "Digite uma palavra ou frase: ";
   cin >> letras;
 
   contador(letras, alfabeto);
 
   cout << '\n';
-  cout << "|--------------------"; << '\n';
+  cout << "|---------------------" << '\n';
   cout << "| A = " << alfabeto[0] << '\n';
   cout << "| B = " << alfabeto[1] << '\n';
   cout << "| C = " << alfabeto[2] << '\n';
@@ -94,7 +100,7 @@ int main(){
   cout << "| X = " << alfabeto[23] << '\n';
   cout << "| Y = " << alfabeto[24] << '\n';
   cout << "| Z = " << alfabeto[25] << '\n';
-  cout << "|--------------------"; << '\n';
+  cout << "|---------------------" << '\n';
 
   return 0;
 }
