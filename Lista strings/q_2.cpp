d3e121cc1c5ca24f0e3d5ead97112a3f22ad5b46
Carol <caroldazevedo52@gmail.com>
2025-07-28 21:40:43 -0300

    Update q_2.cpp

--- Lista strings/q_2.cpp
+++ Lista strings/q_2.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <cstring>
+
 using namespace std;
 
 int main(){
-  char letras[1000];
+  char letras[1000], space[4]="a b";
   cout << "Digite uma palavra ou frase: ";
-  cin.getline(letras);
+  cin.getline(letras, 1000);
 
   for(int i=0; i<strlen(letras); i++){
-    if(letras[i]==" "){
+    if(letras[i]==space[1]){
       cout << '\n';
     }
     else{
