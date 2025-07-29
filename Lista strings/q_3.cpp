a0204514f016957c50dfd37a29c62050f0b8e57f
Carol <caroldazevedo52@gmail.com>
2025-07-28 21:03:05 -0300

    Update q_3.cpp

--- Lista strings/q_3.cpp
+++ Lista strings/q_3.cpp
@@ -1,33 +1,39 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-str palindromo(char letras[]){
-  int tamanho = strlen(letras)-1, check=0;
-  int metade = tamanho/2
-  char div1[metade], div2[metade];
+int palindromo(char letras[]){
+    int tamanho = strlen(letras), check=0;
+    int metade = (tamanho)/2;
 
-  for(int i=0; i<metade; i++){
-    if(div1[i] != div2[metade-i-1]){
-      check++
+    for(int i=0; i<metade; i++){
+        if(letras[i] != letras[(tamanho-1)-i]){
+            check++;
+        }
     }
-  }
 
-  if(check==0){
-    return "Verdadeiro";
-  }
-  else{
-    return "Falso";
-  }
+    if(check==0){
+        return 1;
+    }
+    else{
+        return 2;
+    }
 }
 
 int main(){
   char letras[1000];
-  str vf;
+  int vf;
   cout << "Digite uma palavra ou frase: ";
   cin >> letras;
 
   vf = palindromo(letras);
-  cout << '\n' << vf;
   
-  return 0
+  if(vf==1){
+    cout << '\n' << "Verdadeiro";
+  }
+  else if(vf==2){
+    cout << '\n' << "Falso";
+  }
+  
+  return 0;
 }
