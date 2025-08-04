f6eacb29a72ff8ad9559c786ce279611580fc634
Carol <caroldazevedo52@gmail.com>
2025-08-04 14:15:02 -0300

    Update Merge.cpp

--- Projeto_Avaliativo/Merge.cpp
+++ Projeto_Avaliativo/Merge.cpp
@@ -2,22 +2,18 @@
 #include <iostream>
 using namespace std;
 
-void merge(int array[], int left, 
-                     int mid, int right)
-{
+void merge(int array[], int left, int mid, int right){
     int n1 = mid - left + 1;
     int n2 = right - mid;
-
     vector<int> L(n1), R(n2);
 
     for (int i = 0; i < n1; i++)
         L[i] = array[left + i];
     for (int j = 0; j < n2; j++)
         R[j] = array[mid + 1 + j];
-
     int i = 0, j = 0;
     int k = left;
-
+  
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
             array[k] = L[i];
@@ -29,13 +25,11 @@ void merge(int array[], int left,
         }
         k++;
     }
-
     while (i < n1) {
         array[k] = L[i];
         i++;
         k++;
     }
-
     while (j < n2) {
         array[k] = R[j];
         j++;
