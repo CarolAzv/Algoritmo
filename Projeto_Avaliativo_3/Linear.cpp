3fad792839b18dccf50a57e911d61dadc79d3941
Carol <caroldazevedo52@gmail.com>
2025-08-04 13:34:26 -0300

    Update Linear.cpp

--- Projeto_Avaliativo_3/Linear.cpp
+++ Projeto_Avaliativo_3/Linear.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+void randomArray(int array[], int tamanho){
+  srand(time(0));
+  for (int i = 0; i < tamanho; ++i){
+    array[i] = rand() % 1001; 
+  }
+}
+
 int main(){
     int n;
     cout << "Informe o tamanho da lista: "; // << "\n";
