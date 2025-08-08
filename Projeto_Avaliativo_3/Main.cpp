56ae7545bc49e2651fca56dd346c1fa7d947fbfb
Carol <caroldazevedo52@gmail.com>
2025-08-08 18:59:13 -0300

    Update Main.cpp

--- Projeto_Avaliativo_3/Main.cpp
+++ Projeto_Avaliativo_3/Main.cpp
@@ -15,6 +15,69 @@ int randomNumFromArray(int tamanho, int array[]){
     return array[randomNumber];
 }
 
+void merge(int array[], int left, int mid, int right){
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    vector<int> L(n1), R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = array[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = array[mid + 1 + j];
+    int i = 0, j = 0;
+    int k = left;
+  
+    while (i < n1 && j < n2){
+        if (L[i] <= R[j]) {
+            array[k] = L[i];
+            i++;
+        }
+        else {
+            array[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1){
+        array[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2){
+        array[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int array[], int left, int right){
+    if (left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+    mergeSort(array, left, mid);
+    mergeSort(array, mid + 1, right);
+    merge(array, left, mid, right);
+}
+
+
+int binarioI(int array[], int tamanho, int want){
+  int meio, comeco=0, fim=tamanho-1;
+  while(comeco<=fim){
+    meio = comeco+(fim-comeco)/2;
+    if(array[meio]==want){
+      return meio;
+    }
+    if(want < array[meio]){
+      fim = meio-1;
+    }
+    else if(want > array[meio]){
+      comeco = meio+1;
+    }
+  }
+  return -1;
+}
+
 int linearI(int array[], int tamanho, int want){
     for(int i=0; i<tamanho; i++){
         if(array[i]==want){
